ac88074cd335d9ee016e0c754b0bacbafef1be2d
Rian Alves <rianalves2009.ra@gmail.com>
2019-06-27 14:28:07 -0300

    Implementação da classe Vinculo

---
+++ src/main/java/br/ufjf/dcc193/trabalho03/model/Vinculo.java
@@ -0,0 +1,63 @@
+package br.ufjf.dcc193.trabalho03.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Vinculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @ManyToOne
+    private Item itemOrigem;
+    @ManyToOne
+    private Item itemDestino;
+    @OneToMany
+    List<Etiqueta> etiquetas;
+    @OneToMany
+    List<Anotacao> anotacoes; 
+
+    public Long getId() {
+        return id;
+    }
+   
+    public Item getItemOrigem() {
+        return itemOrigem;
+    }
+ 
+    public Item getItemDestino() {
+        return itemDestino;
+    }
+ 
+    public List<Etiqueta> getEtiquetas() {
+        return etiquetas;
+    }
+
+    public List<Anotacao> getAnotacoes() {
+        return anotacoes;
+    }
+    
+  
+    public void setItemOrigem(Item itemOrigem) {
+        this.itemOrigem = itemOrigem;
+    }
+
+    public void setItemDestino(Item itemDestino) {
+        this.itemDestino = itemDestino;
+    }
+  
+    public void setEtiquetas(List<Etiqueta> etiquetas) {
+        this.etiquetas = etiquetas;
+    }
+  
+    public void setAnotacoes(List<Anotacao> anotacoes) {
+        this.anotacoes = anotacoes;
+    }
+}
